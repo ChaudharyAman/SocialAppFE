38eb2978687021233e53017cb0374363bbb848e9
Aman <tomaraman50@gmail.com>
2025-09-23 11:47:53 +0530

    Refactor import paths: Update API import statements to use relative paths for consistency across slices

--- src/Redux/Slices/loggedInUserSlice.jsx
+++ src/Redux/Slices/loggedInUserSlice.jsx
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import api from "../Api/api";
+import api from "../../Api/api.js";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
