c43ab3ef85322f93f8cc46b913e5469e19a76c63
Aman <tomaraman50@gmail.com>
2025-09-23 12:53:43 +0530

    Refactor PrivateRoute to remove token handling from localStorage in API call

--- src/Components/privateRoute.jsx
+++ src/Components/privateRoute.jsx
@@ -12,20 +12,15 @@ const PrivateRoute = () => {
       console.log("useEffect me aya")
       try {
         console.log("try me aya")
-        // Get token from localStorage
-        const token = localStorage.getItem("token");
-        console.log("token: ", token)
         const res = await api.get(`${API_BASE_URL}/api/v1/checkAuth`, {
           withCredentials: true,
-          headers: {
-            Authorization: token ? `Bearer ${token}` : undefined,
-          },
-        });     
+        });
         console.log("set ke uppar me aya")
-        console.log("resu :: ", res)
+        console.log("res :: ", res)
         setAuth(res?.data?.success);
         console.log("set ke niche me aya")
-      } catch {
+      } 
+      catch {
         console.log("catch me aya")
         setAuth(false);
       }
