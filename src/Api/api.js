b79eed7848886a96eb018a44011459c83b6ff4d1
Aman <tomaraman50@gmail.com>
2025-09-23 11:57:55 +0530

    Add logging for request configuration in API interceptor

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,4 +1,4 @@
-
+import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -8,6 +8,7 @@ api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
     if (token) {
+      console.log(config)
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
