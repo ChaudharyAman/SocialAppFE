46c6f957af50492744a77fad99c1ffce382ac0ae
Aman <tomaraman50@gmail.com>
2025-09-23 13:00:47 +0530

    Refactor API interceptor to improve error handling and response management

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,20 +1,54 @@
 import axios from "axios";
-
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
+    if (!token) {
+      return window.location.href("/login");
+    }
     if (token) {
-      console.log(config)
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => Promise.reject(error)
+  (error) => {
+    return Promise.reject(error);
+  }
 );
-
+api.interceptors.response.use(
+  (response) => {
+    if (response.data.statusCode === 419) {
+      localStorage.removeItem("token");
+      return (window.location.href = "/login");
+    }
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
 export default api;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
